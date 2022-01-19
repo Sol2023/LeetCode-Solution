31044767248e479355a89cae0ea87232500ae912
Shouzheng Huang <92194263+Sol2023@users.noreply.github.com>
2022-01-19 09:15:51 -0600

    Time: 28 ms (88.87%), Space: 14.3 MB (41.44%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -1,9 +1,15 @@
 class Solution:
-    def fib(self, n: int, memorize = {0:0,1:1}) -> int:
+    def fib(self, n: int) -> int:
+        memorize = {0:0,1:1}
+        return self.getNFib(n, memorize)
+    
+    def getNFib(self, n, memorize) :
         if n in memorize:
             return memorize[n]
         else:
-            memorize[n]=self.fib(n-1, memorize) + self.fib(n-2, memorize)
+            memorize[n]=self.getNFib(n-1, memorize) + self.getNFib(n-2, memorize)
             return memorize[n]
+    
+        
         
         
