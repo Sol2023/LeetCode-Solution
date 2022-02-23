5370f5ff15091cca193a8897f07cd0a1931375e0
Shouzheng Huang <92194263+Sol2023@users.noreply.github.com>
2022-02-23 12:20:03 -0600

    Time: 466 ms (52.03%), Space: 0B (100.00%) - LeetHub

--- 197-rising-temperature/197-rising-temperature.sql
+++ 197-rising-temperature/197-rising-temperature.sql
@@ -1,10 +1,17 @@
 # Write your MySQL query statement below
 
-select w2.id
-from 
-    Weather w1,
-    Weather w2
-where 
-    datediff(w1.recordDate, w2.recordDate)=-1 
-    and w1.temperature < w2.temperature
+# select w2.id
+# from 
+#     Weather w1,
+#     Weather w2
+# where 
+#     datediff(w1.recordDate, w2.recordDate)=-1 
+#     and w1.temperature < w2.temperature
 
+SELECT
+    weather.id AS 'Id'
+FROM
+    weather
+        JOIN
+    weather w ON DATEDIFF(weather.recordDate, w.recordDate) = 1
+        AND weather.Temperature > w.Temperature
