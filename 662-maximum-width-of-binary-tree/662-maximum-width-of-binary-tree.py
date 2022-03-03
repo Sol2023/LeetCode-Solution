a59a9303d51083252f33fb63d33a50ee4b47ab44
Shouzheng Huang <92194263+Sol2023@users.noreply.github.com>
2022-03-03 09:48:28 -0600

    Time: 40 ms (97.65%), Space: 14.9 MB (54.98%) - LeetHub

--- 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.py
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.py
@@ -1,29 +1,29 @@
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 class Solution:
     def widthOfBinaryTree(self, root: Optional[TreeNode]) -> int:
         if not root:
             return 0
-        
-        max_width = 0
         queue = deque()
+        maxWidth = 0
         queue.append((root, 0))
         
         while queue:
-            current_level_length = len(queue)
-            node, level_head_index = queue[0]
+            length = len(queue)
+            node, levelHeadIdx = queue[0]
             
-            for i in range(current_level_length):
-                node, col_index = queue.popleft()
+            for i in range(length):
+                node, currLevelIdx = queue.popleft()
                 if node.left:
-                    queue.append((node.left, 2*col_index))
+                    queue.append((node.left, 2*currLevelIdx))
                 if node.right:
-                    queue.append((node.right, 2*col_index+1))
-            max_width = max(max_width, col_index-level_head_index+1)
-        
-        return max_width
-        
+                    queue.append((node.right, 2*currLevelIdx+1))
+                    
+            maxWidth = max(maxWidth,currLevelIdx-levelHeadIdx+1)
+
+        return maxWidth
+    	
