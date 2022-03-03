e25baa79ea60566b990e2594c5ebc67f0e47e87d
Shouzheng Huang <92194263+Sol2023@users.noreply.github.com>
2022-03-03 10:14:35 -0600

    Time: 268 ms (72.28%), Space: 14.9 MB (78.16%) - LeetHub

--- 926-flip-string-to-monotone-increasing/926-flip-string-to-monotone-increasing.py
+++ 926-flip-string-to-monotone-increasing/926-flip-string-to-monotone-increasing.py
@@ -3,7 +3,6 @@ class Solution:
         one =0
         flip =0
         for i in range(len(s)):
-            print(i, s[i], one, flip)
             if s[i]=='1':
                 one+=1
             else:
