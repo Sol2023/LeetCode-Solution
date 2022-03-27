b82f627288af4b1909f5710bf2c45b982136e375
Shouzheng Huang <92194263+Sol2023@users.noreply.github.com>
2022-03-26 20:11:01 -0500

    Time: 69 ms (5.06%), Space: 13.9 MB (23.18%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.py
+++ 118-pascals-triangle/118-pascals-triangle.py
@@ -3,6 +3,6 @@ class Solution:
         ans=[[1]]
         for i in range(1,numRows):
             ans.append(list(map(lambda x,y: x+y, ans[-1]+[0], [0]+ ans[-1])))
-        return ans[:numRows]
+        return ans
 
         
