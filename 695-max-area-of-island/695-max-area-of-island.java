699f2657f5fd36b4cbadb8d3673e0982bb2b22ab
Shouzheng Huang <92194263+Sol2023@users.noreply.github.com>
2022-04-03 09:40:01 -0500

    Time: 3 ms (75.25%), Space: 46.6 MB (79.30%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.java
+++ 695-max-area-of-island/695-max-area-of-island.java
@@ -1,15 +1,26 @@
-class Solution:
-    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        m, n = len(grid), len(grid[0])
-        
-        def explore(r, c):
-            if  0<=r<m and 0<=c<n and grid[r][c]:
-                grid[r][c]=0
-                return 1+explore(r-1,c)+explore(r+1,c)+explore(r,c-1)+explore(r,c+1)
-            return 0
-        
-        areas = [explore(i,j) for i in range(m) for j in range(n) if grid[i][j]]
-        
-        return max(areas) if areas else 0
+class Solution {
+    public int maxAreaOfIsland(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int maxArea = 0;
+        for(int i =0; i<m; i++){
+            for(int j =0; j<n;j++){
+                if(grid[i][j]==1){
+                    maxArea = Math.max(maxArea, explore(grid,i,j));
+                }
+                
+            }
             
-        
+        }
+        return maxArea;
+    }
+    
+    public int explore(int[][] grid, int i, int j){
+        if(i>=0 && i<grid.length && j>=0 && j<grid[0].length && grid[i][j]==1){
+            grid[i][j]=0;
+            return 1+ explore(grid, i-1,j)+explore(grid,i+1,j)+explore(grid,i,j-1)+explore(grid,i,j+1);
+            
+        }
+        return 0;
+    }
+}
